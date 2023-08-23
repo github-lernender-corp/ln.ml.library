3f7c93c8c49088fab3f3693bd1d5d09d02327012
Shawn Lovelidge <github@lernendercorp.com>
2023-08-23 12:09:19 -0500

    Committed Code

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     packages=find_packages(where="src"),
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="git@github.com:github-lernender-corp/ln.ml.core.git",
+    url="git@github.com:github-lernender-corp/ln.ml.library.git",
     author="Lovelidge, Shawn",
     author_email="slovelidge@lernendercorp.com",
     license="MIT",
